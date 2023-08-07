2f2eba614c31846852b0c3d68e892a6d281280ff
Yuvraj Wadavale <90822943+wadavaleyuvraj@users.noreply.github.com>
2023-08-07 19:14:05 +0530

    Add files via upload

---
+++ adminscript.js
@@ -0,0 +1,19 @@
+// Function to handle the login form submission
+function handleLogin() {
+    // Get the values from the input fields
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+  
+    // Perform backend validation and authentication
+    // Replace this code with your actual backend logic
+  
+    // Simulate a successful login for demonstration purposes
+    if (email && password) {
+      alert("Login successful");
+      // Redirect the user to the home page or another page
+      window.location.href = "adminindex.html";
+    } else {
+      alert("Invalid email or password. Please try again.");
+    }
+  }
+  
